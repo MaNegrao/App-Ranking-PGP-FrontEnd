4bbebdc41a409ca66c8693af04c5b28cd74c1acf
Matheus Negrão <matheus.dnegrao@gmail.com>
2019-09-02 17:14:58 -0300

    Update script.sql

--- BANCO/script.sql
+++ BANCO/script.sql
@@ -6,7 +6,7 @@ CREATE TABLE player (
     pic bytea,
     password varchar(43),
     nick varchar(50),
-    Id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     wins INTEGER,
     UNIQUE (nick, Id)
 );
