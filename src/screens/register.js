b693c149b3c539832bcbb924696e32d422d33de2
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-16 17:05:57 -0300

    :construction: Adding register screen

---
+++ src/screens/register.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, TextInput, Button } from 'react-native';
+
+import { withFormik } from 'formik';
+
+const Form = (props) => (
+    <View>
+        <TextInput
+            value={props.values.email}
+            onChangeText={text => props.setFieldValue('email', text)}
+        />
+
+        <TextInput
+            value={props.values.checkEmail}
+            onChangeText={text => props.setFieldValue('checkEmail', text)}
+        />
+
+        <TextInput
+            value={props.values.name}
+            onChangeText={text => props.setFieldValue('name', text)}
+        />
+
+        <TextInput
+            value={props.values.nickname}
+            onChangeText={text => props.setFieldValue('nickname', text)}
+            autoCompleteType={'password'}
+        />
+
+        <TextInput
+            value={props.values.password}
+            onChangeText={text => props.setFieldValue('password', text)}
+            autoCompleteType={'password'}
+        />
+
+        <TextInput
+            value={props.values.checkPassword}
+            onChangeText={text => props.setFieldValue('checkPassword', text)}
+            autoCompleteType={'password'}
+        />
+
+        <Button
+            onPress={props.handleSubmit}
+            title="Register"
+        />
+    </View>
+)
+
+export default withFormik({
+    mapPropsToValues: () => ({ email: '', checkEmail: '', name: '', nickname: '', password: '' , checkPassword: ''}),
+  
+    handleSubmit: (values) => {
+      console.log(values);
+    }
+  })(Form);
