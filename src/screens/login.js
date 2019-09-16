49c4034d833dcd01d07e609b86bd98476e8aa213
mvtodescato <matheus.todescato@estudante.uffs.edu.br>
2019-09-16 16:39:00 -0300

    Modificações para chamada do Login na app

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,5 +1,32 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Image } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
-import Counter from '../components/Counter';
-import { isNullLiteralTypeAnnotation, isPipelineBareFunction } from '@babel/types';
+import { View, TextInput, Button } from 'react-native';
+
+import { withFormik } from 'formik';
+
+const Form = (props) => (
+  <View >
+    <TextInput
+      value={props.values.email}
+      onChangeText={text => props.setFieldValue('email', text)}
+    />
+
+    <TextInput
+      value={props.values.password}
+      onChangeText={text => props.setFieldValue('password', text)}
+    />
+
+    <Button
+      onPress={props.handleSubmit}
+      title="Login"
+    />
+  </View>
+);
+
+export default withFormik({
+  mapPropsToValues: () => ({ email: '', password: '' }),
+
+  handleSubmit: (values) => {
+    console.log(values);
+  }
+})(Form);
+
