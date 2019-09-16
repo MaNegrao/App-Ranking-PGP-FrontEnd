0a08386917153c270c6751cf1fcdc88bb4972c13
mvtodescato <matheus.todescato@estudante.uffs.edu.br>
2019-09-16 17:36:03 -0300

    #7 Login screen atualizada

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
-
+import { View, TextInput, Button , StyleSheet,Text,Image} from 'react-native';
 import { withFormik } from 'formik';
 
 const Form = (props) => (
-  <View >
-    <TextInput
-      value={props.values.nick}
-      onChangeText={text => props.setFieldValue('nick', text)}
-    />
+  <View style={styles.container}>
+    <Image source={
+          require('../assets/images/icon.png')
+        }
+        style={styles.welcomeImage
+        }/>
+    <TextInput style = {styles.input} 
+               autoCapitalize="none" 
+               onSubmitEditing={() => this.passwordInput.focus()} 
+               autoCorrect={false} 
+               keyboardType='email-address' 
+               returnKeyType="next" 
+               placeholder='Nickname' 
+               placeholderTextColor='rgba(225,225,225,0.7)'/>
 
-    <TextInput
-      value={props.values.password}
-      onChangeText={text => props.setFieldValue('password', text)}
-    />
+
+    <TextInput style = {styles.input}   
+              returnKeyType="go" 
+              ref={(input)=> this.passwordInput = input} 
+              placeholder='Senha' 
+              placeholderTextColor='rgba(225,225,225,0.7)' 
+              secureTextEntry/>
+
+    
 
     <Button
       onPress={props.handleSubmit}
       title="Login"
+    />
+    <Text>Não tem uma conta?</Text>
+    <Button
+      onPress={props.handleSubmit}
+      title="Cadastre-se"
     />
   </View>
 );
@@ -30,3 +48,10 @@ export default withFormik({
   }
 })(Form);
 
+
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
