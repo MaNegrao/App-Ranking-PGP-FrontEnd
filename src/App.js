8432a5ed35e57b6ffe6519bd1e44e3817773c810
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-17 09:01:37 -0300

    :see_no_evil: Update .gitignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import Login from './screens/login';
+import React, {Component} from 'react';
+import LoginScreen from './screens/login';
+import RegisterScreen from '.screens/login';
+import {createStackNavigator, createAppContainer} from 'react-navigation'
+
+const AppNavigator = createStackNavigator (
+  {
+    Login: {
+      screen : Login
+    }
+
+
+  }
+);
+
+
+
+
 export default function App() {
   return (
     <Login/>
