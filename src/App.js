7da5cc189081c7163a8ddf2c2d3e1f8194c2d924
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-17 11:18:05 -0300

    #6 #7 Navigation created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, {Component} from 'react';
 import LoginScreen from './screens/login';
-import RegisterScreen from '.screens/login';
-import {createStackNavigator, createAppContainer} from 'react-navigation'
+import RegisterScreen from './screens/register';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 const AppNavigator = createStackNavigator (
   {
     Login: {
-      screen : Login
+      screen : LoginScreen
+    },
+
+    Register: {
+      screen: RegisterScreen
     }
+  },
 
-
+  {
+    initialRouteName: 'Login'
   }
 );
 
+const AppContainer = createAppContainer (AppNavigator);
 
-
-
-export default function App() {
-  return (
-    <Login/>
-  );
+export default class App extends Component{
+  render(){
+    return (
+      <AppContainer/>
+    )
+  }
 }
