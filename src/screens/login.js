ae9f878160cf14d43865ca5b53b4a2f57a975fcb
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-17 11:18:54 -0300

    #7 login finished

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,57 +1,67 @@
 import React from 'react';
-import { View, TextInput, Button , StyleSheet,Text,Image} from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text, Image} from 'react-native';
 import { withFormik } from 'formik';
 
-const Form = (props) => (
+const loginForm = (props) => (
   <View style={styles.container}>
+
     <Image source={
-          require('../assets/images/icon.png')
-        }
-        style={styles.welcomeImage
-        }/>
-    <TextInput style = {styles.input} 
-               autoCapitalize="none" 
-               onSubmitEditing={() => this.passwordInput.focus()} 
-               autoCorrect={false} 
-               keyboardType='email-address' 
-               returnKeyType="next" 
-               placeholder='Nickname' 
-               placeholderTextColor='rgba(225,225,225,0.7)'/>
+      require('../assets/images/icon.png')
+    }/>
 
-
-    <TextInput style = {styles.input}   
-              returnKeyType="go" 
-              ref={(input)=> this.passwordInput = input} 
-              placeholder='Senha' 
-              placeholderTextColor='rgba(225,225,225,0.7)' 
-              secureTextEntry/>
-
-    
-
-    <Button
-      onPress={props.handleSubmit}
-      title="Login"
+	<TextInput 
+		style = {styles.input}
+		value={props.values.nickname}
+		onChangeText={text => props.setFieldValue('nickname', text)} 
+		returnKeyType="next" 
+		placeholder='Nickname' 
     />
-    <Text>Não tem uma conta?</Text>
+
+    <TextInput 
+		style = {styles.input}   
+		returnKeyType="go" 
+		value={props.values.password}
+		onChangeText={text => props.setFieldValue('password', text)}
+		autoCompleteType={'password'}
+		placeholder='Senha'             
+		secureTextEntry
+    />
+
     <Button
-      onPress={props.handleSubmit}
-      title="Cadastre-se"
+		onPress={props.handleSubmit}
+		title="Login"
+    />
+
+    <Text>Não tem uma conta?</Text>
+    
+    <Button
+		onPress={ () => props.navigation.navigate('Register')}
+		title="Cadastre-se"
     />
   </View>
 );
 
 export default withFormik({
-  mapPropsToValues: () => ({ nick: '', password: '' }),
+	mapPropsToValues: () => ({ nickname: '', password: '' }),
 
-  handleSubmit: (values) => {
-    console.log(values);
-  }
-})(Form);
+	handleSubmit: (values) => {
+		console.log(values);
+	}
 
+})(loginForm);
 
-
-// define your styles
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		padding: 10
+	},
+
+	input: {
+		padding: 20,
+		fontSize: 30,
+		borderRadius: 0,
+		borderWidth: 2,
+		borderColor: 'black',
+	}
+})
