6bc8a8e8e7ef837131d46d6555ddec8b38f1b429
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-23 15:25:09 -0300

    #8 styles

--- src/screens/register.js
+++ src/screens/register.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import api from '../services/api'
 import { View, TextInput, Button, Text, StyleSheet, Image } from 'react-native';
 import ImagePicker from "../components/imagePicker";
-
+import SafeAreaView from 'react-native-safe-area-view';
 
 export default class Register extends Component {
     constructor() {
@@ -37,77 +37,78 @@ export default class Register extends Component {
 
     render () {
         return(
-            <View>
-                <Text style={styles.text}>Bem Vindo, Registre-se</Text>
-                
-                <Image source={
-                require('../assets/images/icon.png')
-                }/>
-                
-                <TextInput
-                    style = {styles.input} 
-                    placeholder={'Nome'}
-                    value={this.state.name}
-                    onChangeText={(name) => this.setState({name})}
-                    autoCompleteType={'name'}
-                />
-        
-                <TextInput
-                    style = {styles.input} 
-                    placeholder={'Nickname'}
-                    value={this.state.nick}
-                    onChangeText={(nick) => this.setState({nick})}
-                />
-        
-                <TextInput
-                    style = {styles.input} 
-                    placeholder={'E-mail'}
-					value={this.state.email}
-                    onChangeText={(email) => this.setState({email})}
-                    autoCompleteType={'email'}
-                    keyboardType={'email-address'}
-                />
-        
-                {/* <TextInput
-                    style = {styles.input} 
-                    placeholder={'Confirme o E-mail'}
-                    value={this.state.checkEmail}
-                    onChangeText={(checkEmail) => this.setState({checkEmail})}
-                    autoCompleteType={'email'}
-                    keyboardType={'email-address'}
-                /> */}
-        
-                <TextInput
-                    style = {styles.input} 
-                    placeholder={'Senha'}
-                    value={this.state.password}
-                    onChangeText={(password) => this.setState({password})}
-                    autoCompleteType={'password'}
-					secureTextEntry
+			<SafeAreaView style={styles.container}>
+                <View>
+                    <View style={styles.header}>
+                        <Text style={styles.title}>Bem Vindo, Registre-se</Text>
+                        <Image source={require('../assets/images/icon.png')}/>
+                    </View>
+                    
+                    <TextInput
+                        style = {styles.input} 
+                        placeholder={'Nome'}
+                        value={this.state.name}
+                        onChangeText={(name) => this.setState({name})}
+                        autoCompleteType={'name'}
                     />
-        
-                {/* <TextInput
-                    style = {styles.input} 
-                    placeholder={'Confirme a Senha'}
-                    value={this.state.checkPassword}
-                    onChangeText={(checkPassword) => this.setState({checkPassword})}
-                    autoCompleteType={'password'}
-                /> */}
-        
-                <ImagePicker style = {styles.input}/>
-        
-                <Button
-                    onPress={this.registerSubmit.bind(this)}
-                    title="Register"
-                />
-        
-                <Text>Já tenho uma conta:</Text>
-        
-                <Button
-                    onPress={ () => this.props.navigation.navigate('Login')}
-                    title="Login"
-                />
-            </View>
+            
+                    <TextInput
+                        style = {styles.input} 
+                        placeholder={'Nickname'}
+                        value={this.state.nick}
+                        onChangeText={(nick) => this.setState({nick})}
+                    />
+            
+                    <TextInput
+                        style = {styles.input} 
+                        placeholder={'E-mail'}
+                        value={this.state.email}
+                        onChangeText={(email) => this.setState({email})}
+                        autoCompleteType={'email'}
+                        keyboardType={'email-address'}
+                    />
+            
+                    {/* <TextInput
+                        style = {styles.input} 
+                        placeholder={'Confirme o E-mail'}
+                        value={this.state.checkEmail}
+                        onChangeText={(checkEmail) => this.setState({checkEmail})}
+                        autoCompleteType={'email'}
+                        keyboardType={'email-address'}
+                    /> */}
+            
+                    <TextInput
+                        style = {styles.input} 
+                        placeholder={'Senha'}
+                        value={this.state.password}
+                        onChangeText={(password) => this.setState({password})}
+                        autoCompleteType={'password'}
+                        secureTextEntry
+                        />
+            
+                    {/* <TextInput
+                        style = {styles.input} 
+                        placeholder={'Confirme a Senha'}
+                        value={this.state.checkPassword}
+                        onChangeText={(checkPassword) => this.setState({checkPassword})}
+                        autoCompleteType={'password'}
+                    /> */}
+            
+                    <ImagePicker style = {styles.input}/>
+            
+                    <Button
+                        onPress={this.registerSubmit.bind(this)}
+                        title="Register"
+                    />
+            
+                    <Text>Já tenho uma conta:</Text>
+            
+                    <Button
+                        onPress={ () => this.props.navigation.navigate('Login')}
+                        title="Login"
+                    />
+                </View>
+            </SafeAreaView>
         )
 
     }
@@ -121,6 +122,14 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         padding: 10
+    },
+
+    header: {
+        fontFamily: 'Nunito-Reqular',
+        fontSize: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
     },
 
     input: {
